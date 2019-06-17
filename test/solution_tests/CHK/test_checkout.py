af3528097c144db3b63d7c7e6e6f70ce6aab8d6d
sbx_user1051 <sbx_user1051@169.254.9.29>
2019-06-17 13:31:28 +0000

    Mon Jun 17 13:31:28 UTC 2019

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -9,5 +9,18 @@ class TestCheckout():
     def test_checkout_bad_input(self):
         assert checkout_solution.checkout("AAAABBBCCDD23") == -1
 
-    def test_checkout_empt_input(self):
-        assert checkout_solution.checkout("") == -1
+    def test_checkout_A(self):
+        input_1 = "A"
+        input_2 = "AA"
+        input_3 = "AAA"
+        input_4 = "AAAA"
+        assert checkout_solution.checkout(input_1) == 50
+        assert checkout_solution.checkout(input_2) == 100
+        assert checkout_solution.checkout(input_3) == 130
+        assert checkout_solution.checkout(input_4) == 180
+
+    def test_checkout_empty(self):
+        assert checkout_solution.checkout(" ") == -1
+
+    def test_checkout_no_deal(self):
+        assert checkout_solution.checkout("CCCDDD") == 105
